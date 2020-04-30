a47705777d087293a065a161a7f88a2474cc0fea
talesb <talesbatistafig@hotmail.com>
2020-04-30 00:38:45 -0300

    Adicionando testes do serviço de token (refatorando utilizando beforeEach e afterEach)

--- alurapic/src/app/core/token/token.service.spec.ts
+++ alurapic/src/app/core/token/token.service.spec.ts
@@ -2,6 +2,9 @@ import { TokenService } from "./token.service";
 
 describe('O serviço TokenService', () => {
 
+    let token;
+    let tokenService;
+
     it('Deve ser instanciado', () => {
         const tokenService = new TokenService();
         expect(tokenService).toBeTruthy();
@@ -9,12 +12,28 @@ describe('O serviço TokenService', () => {
 
 
     it('Deve guardar um token', () => {
-        const token = 'testetoken'
-        const tokenService = new TokenService();
         tokenService.setToken(token);
         expect(tokenService.getToken()).toBe('testetoken');
-        expect(tokenService.hasToken()).toBeTruthy(); 
+        expect(tokenService.hasToken()).toBeTruthy();
     });
+
+
+    it('Deve remover um token', () => {
+        tokenService.setToken(token);
+        tokenService.removeToken();
+        expect(tokenService.hasToken()).toBeFalsy();
+        expect(tokenService.getToken()).toBeFalsy();
+    });
+
+
+    afterEach(() => {
+        localStorage.clear();
+    })
+
+    beforeEach(() => {
+        token = 'testetoken';
+        tokenService = new TokenService();
+    })
 
 
 });
