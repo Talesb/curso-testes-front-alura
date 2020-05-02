9d774b8a0753c467ec4583156ea741f6456901b4
talesb <talesbatistafig@hotmail.com>
2020-05-02 17:46:24 -0300

    Criando testes no compoente de footer, trabalhando com fixture

---
+++ alurapic/src/app/core/footer/footer.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, async } from "@angular/core/testing"
+import { FooterComponent } from "./footer.component"
+import { RouterTestingModule } from "@angular/router/testing";
+import { UserService } from "../user/user.service";
+import { of } from "rxjs";
+
+describe('O component de Footer', () => {
+
+    let component: FooterComponent;
+
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            providers: [UserService],
+            declarations: [FooterComponent]
+        }).compileComponents();
+    }));
+
+
+    beforeEach(() => {
+        const userService = TestBed.get(UserService);
+      
+        spyOn(userService, "getUser").and.returnValue(of({
+            email: 'alvaro@alvaro.com',
+            name: 'Alvaro',
+            id: 1
+        }));
+
+       
+        const fixture = TestBed.createComponent(FooterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+
+
+    it('Deve ser instanciado', () => {
+        expect(component).toBeTruthy();
+    })
+
+
+
+})
